ad9000a6ba738af735d82427572f196cb535a7ca
philipp-gatzka <phil.gatzka@gmail.com>
2025-12-23 15:20:54 +0100

    #34 Fix widget_test.dart to work with HomeScreen providers

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:sodium/app.dart';
+import 'package:sodium/models/recipe.dart';
+import 'package:sodium/providers/recipe_provider.dart';
 
 void main() {
   testWidgets('App renders correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(const SodiumApp());
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          recipesProvider.overrideWith((ref) async => <Recipe>[]),
+        ],
+        child: const SodiumApp(),
+      ),
+    );
 
-    expect(find.text('Sodium Recipe App'), findsOneWidget);
+    await tester.pumpAndSettle();
+
+    expect(find.text('My Recipes'), findsOneWidget);
   });
 }
